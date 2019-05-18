8e941fce7800666daaf944822d01a5682e735db5
Warut Surapat <s.warut@gmail.com>
2019-05-18 23:38:08 +0900

    Bring back test.

--- test/ex_realworld_web/controllers/api/user_controller_test.exs
+++ test/ex_realworld_web/controllers/api/user_controller_test.exs
@@ -10,13 +10,6 @@ defmodule ExRealworldWeb.Api.UserControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  describe "index" do
-    test "lists all users", %{conn: conn} do
-      conn = get(conn, Routes.api_user_path(conn, :index))
-      assert json_response(conn, 200) == %{"users" => []}
-    end
-  end
-
   describe "create user" do
     test "creates and returns user if data is valid", %{conn: conn} do
       conn = post(conn, Routes.api_user_path(conn, :create, user: @valid_user_attributes))
@@ -34,6 +27,18 @@ defmodule ExRealworldWeb.Api.UserControllerTest do
     test "with wrong credential, get error", %{conn: conn} do
       conn = post(conn, Routes.api_user_path(conn, :create, user: @valid_user_attributes))
       conn = post(conn, Routes.api_login_path(conn, :login, user: Map.merge(@valid_user_attributes, %{password: "2"})))
+      assert %{"user" => user} = json_response(conn, 200)
+      assert user["email"] == @valid_user_attributes.email
+    end
+  end
+
+  describe "index" do
+    test "returns current user", %{conn: conn} do
+      conn = post(conn, Routes.api_user_path(conn, :create, user: @valid_user_attributes))
+      conn = post(conn, Routes.api_login_path(conn, :login, user: @valid_user_attributes))
+      %{"user" => user} = json_response(conn, 200)
+
+      conn = get(conn, Routes.api_user_path(conn, :index))
       assert %{"user" => user} = json_response(conn, 200)
       assert user["email"] == @valid_user_attributes.email
     end
