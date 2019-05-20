b3d8a09338cbf6a371716a7887b08af454394c77
Warut Surapat <s.warut@gmail.com>
2019-05-20 23:49:38 +0900

    Refactor auth.

--- lib/ex_realworld_web/controllers/api/auth.ex
+++ lib/ex_realworld_web/controllers/api/auth.ex
@@ -7,17 +7,15 @@ defmodule ExRealworldWeb.Api.Auth do
 
   def call(conn, _opts) do
     token = get_req_header(conn, "authorization")
-    case token do
-      [] -> ""
-      [token] ->
-        case ExRealworldWeb.UserToken.decode_and_verify(token) do
-          {:ok, claims} ->
-            {:ok, resource} = ExRealworldWeb.UserToken.resource_from_claims(claims)
-            user = Accounts.authenticate_with_email_and_token(resource.email, token)
-            IO.puts("User----  = #{inspect user}")
-            conn |> assign(:current_user, user)
-          {:error, errors} -> IO.puts("ERRORS----  = #{inspect errors}")
-        end
+    with [token] <- get_req_header(conn, "authorization") do
+      case ExRealworldWeb.UserToken.decode_and_verify(token) do
+        {:ok, claims} ->
+          {:ok, resource} = ExRealworldWeb.UserToken.resource_from_claims(claims)
+          user = Accounts.authenticate_with_email_and_token(resource.email, token)
+          IO.puts("User----  = #{inspect user}")
+          conn |> assign(:current_user, user)
+        {:error, errors} -> IO.puts("ERRORS----  = #{inspect errors}")
+      end
     end
 
     IO.puts("CONN PARAM = #{inspect conn.params}")
