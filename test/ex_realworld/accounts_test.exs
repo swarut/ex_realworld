b0b2885af3e3199bc081943b1db6e7890255acc3
Warut Surapat <wsurapat@gilt.jp>
2019-05-22 14:45:45 +0900

    Add more test for unique email validation.

--- test/ex_realworld/accounts_test.exs
+++ test/ex_realworld/accounts_test.exs
@@ -42,6 +42,11 @@ defmodule ExRealworld.AccountsTest do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
     end
 
+    test "create_user/1 with non-unique email returns error changeset" do
+      Accounts.create_user(@valid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@valid_attrs)
+    end
+
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
